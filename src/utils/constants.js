0d2242e18947e9d6857f3609d778995d34466e17
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-06 18:28:21 -0300

    feat: create initial constants

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+import { date } from './date.js';
+
+export const CONSTANTS = {
+  appURL: 'https://www.mercadolivre.com.br/mais-vendidos',
+  filePath: `./src/data/${date}.json`,
+  categoryTitleClass: '.dynamic__carousel .dynamic__carousel-title',
+  productCardClass: '.dynamic-carousel__link-container',
+  productNameClass: '.dynamic-carousel__title',
+  productImageClass: '.dynamic-carousel__img',
+  productPriceClass: '.dynamic-carousel__price',
+};
---
+++ src/utils/date.js
@@ -0,0 +1,1 @@
+export const date = new Date().toISOString().substring(0, 10);
