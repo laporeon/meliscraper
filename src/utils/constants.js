8ef415017921b93291b13767e1721d18a63d28ca
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-07 15:53:52 -0300

    feat: add more scraped classes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,12 @@ export const CONSTANTS = {
   appURL: 'https://www.mercadolivre.com.br/mais-vendidos',
   filePath: `./src/data/${date}.json`,
   categoryTitleClass: '.dynamic__carousel .dynamic__carousel-title',
-  productCardClass: '.dynamic-carousel__link-container',
+  productCardClass: '.dynamic-carousel__link-container', // aqui
+  productPositionClass:
+    '.dynamic-carousel__pill-container--text.dynamic-carousel__pill-container--text-best-seller',
   productNameClass: '.dynamic-carousel__title',
   productImageClass: '.dynamic-carousel__img',
-  productPriceClass: '.dynamic-carousel__price',
+  productMainPriceClass: '.dynamic-carousel__price > span',
+  productDecimalPriceClass: '.dynamic-carousel__price > sup',
+  productLinkClass: '.dynamic-carousel__item-container > a',
 };
