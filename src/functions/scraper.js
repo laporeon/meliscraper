c552b8baee95e59018d6ad882fd0d338aaa8b45b
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-11 18:07:45 -0300

    refactor: improve scraping function

--- src/functions/scraper.js
+++ src/functions/scraper.js
@@ -1,5 +1,3 @@
-import fs from 'node:fs/promises';
-
 import { CONSTANTS } from '../utils/constants.js';
 
 import puppeteer from 'puppeteer';
@@ -13,62 +11,69 @@ export const scrap = async () => {
     timeout: 60000,
   });
 
-  const data = await page.evaluate(CONSTANTS => {
-    const categories = Array.from(
-      document.querySelectorAll(CONSTANTS.categoryTitleClass),
+  const categories = await page.evaluate(async CONSTANTS => {
+    const categoriesList = Array.from(
+      document.querySelectorAll('.dynamic__carousel'),
     );
 
-    return categories.map(category => {
-      /**
-       * This functions removes accentuated characters from a string
-       * and replace it for the plain version.
-       */
-      const normalizedCategoryName = category.textContent
+    return categoriesList.map(category => {
+      const element = category.querySelector(CONSTANTS.categoryTitleClass);
+      const elementSibling = element.parentElement.nextElementSibling;
+
+      const name = element.textContent;
+
+      const slug = element.textContent
         .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '');
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '');
 
       const products = Array.from(
-        category.parentElement.nextElementSibling.querySelectorAll(
-          CONSTANTS.productCardClass,
-        ),
+        elementSibling.querySelectorAll(CONSTANTS.productCardClass),
       );
 
-      const mappedProducts = products.map((product, index) => {
-        //TODO find a better way to deal with prices
-        const value = product
-          .querySelector(CONSTANTS.productMainPriceClass)
-          .innerHTML.replace('R$ ', '');
-        const decimal =
-          product.querySelector(CONSTANTS.productDecimalPriceClass)
-            ?.innerHTML || 0;
-
-        const price = `${value}.${decimal}`;
-
-        return {
-          position: index + 1,
-          name: product.querySelector(CONSTANTS.productNameClass).textContent,
-          image: product
-            .querySelector(CONSTANTS.productImageClass)
-            .getAttribute('src'),
-          price: +price,
-          link: product.parentElement.getAttribute('href'),
-        };
-      });
       return {
-        name: category.textContent,
-        slug: normalizedCategoryName
-          .toLowerCase()
-          .replace(/ /g, '-')
-          .replace(/[^\w-]+/g, ''),
+        name,
+        slug,
         quantity: products.length,
-        products: mappedProducts,
+        products: products.map((product, index) => {
+          //TODO find a better way to deal with prices
+          const value = product
+            .querySelector(CONSTANTS.productMainPriceClass)
+            .innerHTML.replace('R$ ', '')
+            .replace('.', '');
+          const decimal =
+            product.querySelector(CONSTANTS.productDecimalPriceClass)
+              ?.innerHTML || 0;
+
+          const price = `${value}.${decimal}`;
+
+          return {
+            position: index + 1,
+            name: product.querySelector(CONSTANTS.productNameClass).textContent,
+            image: product
+              .querySelector(CONSTANTS.productImageClass)
+              .getAttribute('src'),
+            price: +price,
+            link: product.parentElement.getAttribute('href'),
+          };
+        }),
       };
     });
   }, CONSTANTS);
 
-  console.log({ data });
+  categories.forEach(async category => {
+    const { name, slug, quantity, products } = category;
 
-  await fs.writeFile(CONSTANTS.filePath, JSON.stringify(data));
+    console.log({ category });
+
+    products.forEach(async product => {
+      const { position, name, image, price, link } = product;
+
+      console.log({ product });
+    });
+  });
 
   await browser.close();
 };
