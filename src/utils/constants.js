123fca89f1c1b131bd1d070e6b6be084d9e5a195
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-13 13:05:37 -0300

    chore: add categories section class

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,8 @@ import { date } from './date.js';
 export const CONSTANTS = {
   appURL: 'https://www.mercadolivre.com.br/mais-vendidos',
   filePath: `./src/data/${date}.json`,
-  categoryTitleClass: '.dynamic__carousel .dynamic__carousel-title',
+  categoriesSectionClass: '.dynamic__carousel',
+  categoryTitleClass: '.dynamic__carousel-title',
   productCardClass: '.dynamic-carousel__link-container', // aqui
   productPositionClass:
     '.dynamic-carousel__pill-container--text.dynamic-carousel__pill-container--text-best-seller',
