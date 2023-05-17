66c3bdb5fa1c4da4948fae27ac7a67b9d5d4c1ff
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-17 19:44:33 -0300

    feat: create server and home endpoint

--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
-import { scrap } from './functions/scraper.js';
+import express from 'express';
 
-scrap();
+const app = express();
+
+app.get('/', async (req, res) => {
+  res.json({
+    message:
+      'Hello! Welcome to Meliscraper API. To see full documentation, please go to /api-docs',
+  });
+});
+
+app.listen(3333, () => {
+  console.log(`Server is running on port 3333`);
+});
