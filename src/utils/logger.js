8c406d0025ae2b72d356464e4d4801f355dd0246
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-24 19:32:54 -0300

    feat: create transport for mongodb

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,22 @@
 import { pino } from 'pino';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      translateTime: 'SYS:standard',
+const transport = pino.transport({
+  targets: [
+    {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'SYS:standard',
+      },
     },
-  },
+    {
+      target: 'pino-mongodb',
+      options: {
+        uri: process.env.LOGS_DATABASE_URL,
+        database: 'meliscraper',
+        collection: 'logs',
+      },
+    },
+  ],
 });
+
+export const logger = pino(transport);
