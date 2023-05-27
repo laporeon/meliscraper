477a9eca5ac81f3d43b0d4390b523aa96d12510a
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-05-27 16:52:41 -0300

    feat: implement findByDate method

--- src/controllers/scraper-controller.js
+++ src/controllers/scraper-controller.js
@@ -1,3 +1,4 @@
+import { prisma } from '../database/prisma.js';
 import { logger } from '../utils/logger.js';
 import { scraper } from '../utils/scraper.js';
 
@@ -10,20 +11,61 @@ export class ScrapController {
     try {
       logger.info('New scraping requested.');
 
-      const data = await scraper();
+      const result = await scraper();
 
-      logger.info('Scraping successfully performed.');
+      await categoryController.mapCategories(result.categories);
 
-      categoryController.mapCategories(data.categories);
+      logger.info(
+        'Scraping successfully performed and data saved into database.',
+      );
 
       return res.status(200).json({
         status: 'success',
-        message: 'Scraping successfully performed.',
-        data,
+        message:
+          'Scraping was successfully performed and current data saved into database.',
+        result,
       });
     } catch (err) {
       logger.error({
-        message: 'Error from scraping request',
+        message: 'Error from [[ /scraping ]] request',
+        error: err,
+      });
+      return res.status(500).json({ error: err.message });
+    }
+  }
+
+  async getByDate(req, res) {
+    try {
+      const { date } = req.params;
+
+      const result = await prisma.category.findMany({
+        where: {
+          scrapings: {
+            has: new Date(date),
+          },
+        },
+        select: {
+          name: true,
+          slug: true,
+          products: {
+            select: {
+              name: true,
+              link: true,
+            },
+          },
+        },
+      });
+
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          date,
+          categories: result,
+        },
+      });
+    } catch (err) {
+      logger.error({
+        message: 'Error from [[ /scraping/:date ]] request',
         error: err,
       });
       return res.status(500).json({ error: err.message });
