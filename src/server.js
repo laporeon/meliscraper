96647ed64e06bbe84ac82154a468c268bae16fa0
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2023-06-05 20:24:47 -0300

    feat: configure cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
@@ -11,17 +12,24 @@ import {
 } from './routes/index.js';
 import { logger } from './utils/logger.js';
 
+const PORT = process.env.PORT | 3000;
+const swaggerDocs = JSON.parse(readFileSync('./docs/swagger.json'));
+
+const corsOptions = {
+  origin: ['http://localhost:${PORT}', 'https://meliscraper.onrender.com/'],
+  methods: 'GET,HEAD,POST',
+  optionsSuccessStatus: 204,
+};
+
 const app = express();
 
-const PORT = process.env.PORT | 3000;
+app.use(cors(corsOptions));
 
-const swaggerDocs = JSON.parse(readFileSync('./docs/swagger.json'));
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/scrapings', scrapRoutes);
 app.use('/categories', categoriesRoutes);
 app.use('/products', productsRoutes);
-
-app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', async (req, res) => {
   res.json({
