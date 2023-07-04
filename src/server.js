79dbf258d11dee52832ac6831c12a8b30fb5a525
laporeon <34722707+laporeon@users.noreply.github.com>
2023-07-04 17:48:14 -0300

    refactor: minimize routes declaration

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
-import { readFileSync } from 'node:fs';
-
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 
-import {
-  scrapRoutes,
-  categoriesRoutes,
-  productsRoutes,
-} from './routes/index.js';
+import routes from './routes/index.js';
 import { logger } from './utils/logger.js';
 
 const PORT = process.env.PORT | 3000;
-const swaggerDocs = JSON.parse(readFileSync('./docs/swagger.json'));
 
 const corsOptions = {
   origin: ['http://localhost:${PORT}', 'https://meliscraper.onrender.com/'],
@@ -25,11 +17,7 @@ const app = express();
 
 app.use(cors(corsOptions));
 
-app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use('/scrapings', scrapRoutes);
-app.use('/categories', categoriesRoutes);
-app.use('/products', productsRoutes);
+app.use(routes);
 
 app.get('/', async (req, res) => {
   res.json({
