31258d2ef090a8a9d4732f117138c9e4aa9029c8
laporeon <34722707+laporeon@users.noreply.github.com>
2023-07-04 17:47:33 -0300

    refactor: change import routes method

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,15 @@
-export * from './scraper-routes.js';
-export * from './categories-routes.js';
-export * from './products-routes.js';
+import { Router } from 'express';
+
+import { categoriesRoutes } from './categories-routes.js';
+import { productsRoutes } from './products-routes.js';
+import { scrapRoutes } from './scraper-routes.js';
+import { swaggerRoutes } from './swagger-routes.js';
+
+const routes = Router();
+
+routes.use('/scrapings', scrapRoutes);
+routes.use('/categories', categoriesRoutes);
+routes.use('/products', productsRoutes);
+routes.use('/', swaggerRoutes);
+
+export default routes;
