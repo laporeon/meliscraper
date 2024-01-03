70288813069fb5dbda4ec882e654837a9d948997
laporeon <34722707+laporeon@users.noreply.github.com>
2024-01-03 18:58:24 -0300

    chore: remove some methods

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -18,27 +18,4 @@ export class ProductController {
 
     return;
   }
-
-  async get(req, res) {
-    try {
-      const products = await prisma.product.findMany({
-        select: {
-          name: true,
-          price: true,
-          link: true,
-        },
-      });
-
-      if (products.length < 1)
-        return res.json({
-          error: 'NOT_FOUND',
-          message:
-            'No products available. Please, perform your first scraping using /scrapings route and try this operation again.',
-        });
-
-      return res.status(200).json({ status: 'OK', data: { products } });
-    } catch (err) {
-      return res.status(500).json({ error: err.message });
-    }
-  }
 }
