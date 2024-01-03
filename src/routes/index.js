8b7dc125bf4136a57682865c0c2410c5e64ca844
laporeon <34722707+laporeon@users.noreply.github.com>
2024-01-03 18:57:02 -0300

    chore: remove products routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import { categoriesRoutes } from './categories-routes.js';
-import { productsRoutes } from './products-routes.js';
 import { scrapRoutes } from './scraper-routes.js';
 import { swaggerRoutes } from './swagger-routes.js';
 
@@ -9,7 +8,6 @@ const routes = Router();
 
 routes.use('/scrapings', scrapRoutes);
 routes.use('/categories', categoriesRoutes);
-routes.use('/products', productsRoutes);
 routes.use('/', swaggerRoutes);
 
 export default routes;
