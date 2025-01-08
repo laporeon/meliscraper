2ce159198efc7ff00c0706e6e3dcf704daf8aa01
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-08 18:48:08 -0300

    feat: add createdAt and updatedAt to all tablets and change price from float to int

---
+++ prisma/migrations/20250108175426_add_created_at_and_updated_at_to_all_tables/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `categories` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `products` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `categories` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "categories" DROP COLUMN "created_at",
+ADD COLUMN     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMPTZ(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "created_at",
+ADD COLUMN     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMPTZ(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "scrapings" ADD COLUMN     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
