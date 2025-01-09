03b50d53fdb98d99a41ae5634b1066a94ab1f9f0
laporeon <175569928+daspeon@users.noreply.github.com>
2025-01-09 17:53:33 -0300

    refactor: improve method to create category

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -3,51 +3,17 @@ import { logger } from '../utils/logger.js';
 import { slugify } from '../utils/slugify.js';
 
 export class CategoryController {
-  async create(name, scrapingId) {
-    if (await this.isPresent(name)) {
-      await prisma.category.update({
-        where: { name },
-        data: {
-          scrapings: {
-            connect: {
-              id: scrapingId,
-            },
-          },
-        },
-      });
-
-      return;
-    }
-
-    await prisma.category.create({
-      data: {
-        name,
-        slug: slugify(name),
-        scrapings: {
-          connect: {
-            id: scrapingId,
-          },
-        },
+  async createOrConnectCategory(category) {
+    const { id } = await prisma.category.upsert({
+      where: { name: category.name },
+      update: {},
+      create: {
+        name: category.name,
+        slug: slugify(category.name),
       },
     });
 
-    logger.info(`Category ${name.toUpperCase()} created!`);
-
-    return;
-  }
-
-  async isPresent(name) {
-    const category = await prisma.category.findFirst({ where: { name } });
-
-    if (category) {
-      logger.warn(
-        `Category ${name.toUpperCase()} is already registered. Skipping creation...`,
-      );
-
-      return true;
-    }
-
-    return false;
+    return id;
   }
 
   async get(req, res) {
@@ -67,23 +33,15 @@ export class CategoryController {
         });
 
       return res.status(200).json({ categories });
-    } catch (err) {
+    } catch (error) {
       logger.error({
-        message: 'Error from /categories request',
-        error: err,
+        url: req.originalUrl,
+        method: req.method,
+        error: error.message,
+        stack: error.stack,
       });
-      return res.json({ error: err.message });
+      return res.json({ error: error.message });
     }
-  }
-
-  async findId(name) {
-    const { id } = await prisma.category.findFirst({
-      where: {
-        name,
-      },
-    });
-
-    return { id };
   }
 
   async findProducts(req, res) {
@@ -117,13 +75,14 @@ export class CategoryController {
       return res.json({
         data: { category: name, products },
       });
-    } catch (err) {
+    } catch (error) {
       logger.error({
-        message: 'Error from /categories/:id/products request',
-        error: err,
-        params: req.params,
+        url: req.originalUrl,
+        method: req.method,
+        error: error.message,
+        stack: error.stack,
       });
-      return res.status(500).json({ status: 'ERROR', message: err.message });
+      return res.json({ error: error.message });
     }
   }
 }
