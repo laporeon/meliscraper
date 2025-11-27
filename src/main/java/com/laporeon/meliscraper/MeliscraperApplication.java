6da2a88fb5647a2fa846ad0687e009eaae559984
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-27 16:10:45 -0300

    chore: upload Spring Boot starter template application to init migration

---
+++ src/main/java/com/laporeon/meliscraper/MeliscraperApplication.java
@@ -0,0 +1,13 @@
+package com.laporeon.meliscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliscraperApplication.class, args);
+	}
+
+}
