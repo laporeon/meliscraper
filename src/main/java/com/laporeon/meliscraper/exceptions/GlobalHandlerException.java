8e416b9fa2fa522b44a637eb837aef81357dff33
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-28 16:35:20 -0300

    feat(exceptions): implement handler for IOException on global handler

---
+++ src/main/java/com/laporeon/meliscraper/exceptions/GlobalHandlerException.java
@@ -0,0 +1,26 @@
+package com.laporeon.meliscraper.exceptions;
+
+import com.laporeon.meliscraper.dtos.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorResponseDTO> handleIOException(IOException ex) {
+        ErrorResponseDTO error = new ErrorResponseDTO(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                "An unexpected error occured",
+                Instant.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
