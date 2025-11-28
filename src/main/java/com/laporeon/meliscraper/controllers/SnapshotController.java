827cb6403c27b9b06de541ad668358dca4ccc886
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-28 16:31:31 -0300

    feat(controllers): start implementation of get snapshot endpoint

--- src/main/java/com/laporeon/meliscraper/controllers/SnapshotController.java
+++ src/main/java/com/laporeon/meliscraper/controllers/SnapshotController.java
@@ -1,16 +1,25 @@
 package com.laporeon.meliscraper.controllers;
 
+import com.laporeon.meliscraper.dtos.SnapshotDTO;
+import com.laporeon.meliscraper.services.SnapshotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/snapshots")
+@RequiredArgsConstructor
 public class SnapshotController {
 
+    private final SnapshotService snapshotService;
+
     @GetMapping
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok().body("Hello, World!");
+    public ResponseEntity<SnapshotDTO> getSnapshot() {
+        SnapshotDTO snapshotDTO = snapshotService.getSnapshot();
+        return ResponseEntity.status(HttpStatus.OK).body(snapshotDTO);
     }
 }
