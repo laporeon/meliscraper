cf1cf82ed91a10878ca442b2875402c3f372e6e9
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-01 15:21:24 -0300

    feat(config): implement swagger configuration file with basic API info

---
+++ src/main/java/com/laporeon/meliscraper/config/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.laporeon.meliscraper.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Meliscraper API")
+                                .description("API for retrieving daily snapshots of Mercado Libre's best sellers.")
+                                .version("1.0.0"));
+    }
+}
