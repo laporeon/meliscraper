01fbcf19f8b9e4066f48bb10ea0fef4f5488f203
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-01 14:54:58 -0300

    feat(entities): add ManyToMany relationship with categories using join table and enable JPAAuditing

--- src/main/java/com/laporeon/meliscraper/entities/Snapshot.java
+++ src/main/java/com/laporeon/meliscraper/entities/Snapshot.java
@@ -6,9 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,16 +20,24 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "snapshots")
+@EntityListeners(AuditingEntityListener.class)
 public class Snapshot {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "snapshot_date")
+    @CreatedDate
+    @Column(name = "snapshot_date", nullable = false, updatable = false)
     private LocalDate snapshotDate;
 
     @CreationTimestamp
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @ManyToMany
+    @JoinTable(name = "snapshot_categories",
+            joinColumns = @JoinColumn(name = "snapshot_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories;
 }
