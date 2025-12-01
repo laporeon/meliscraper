34f54fc77ed2e5d7a1faf535023da52f05975a9c
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-01 15:22:24 -0300

    refactor(dtos): remove createdAt from return data at SnapshotDTO

--- src/main/java/com/laporeon/meliscraper/dtos/SnapshotDTO.java
+++ src/main/java/com/laporeon/meliscraper/dtos/SnapshotDTO.java
@@ -1,9 +1,10 @@
 package com.laporeon.meliscraper.dtos;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record SnapshotDTO(UUID id, LocalDate snapshotDate, LocalDateTime createdAt, List<CategoryDTO> categories) {
+public record SnapshotDTO(UUID id,
+                          LocalDate snapshotDate,
+                          List<CategoryDTO> categories) {
 }
