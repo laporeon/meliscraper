038c261e4a3a5b2aed48a3d6889d2b6fff34a665
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-02 11:54:53 -0300

    refactor(repositories): add data as param on findbysnapshotdate

--- src/main/java/com/laporeon/meliscraper/repositories/SnapshotRepository.java
+++ src/main/java/com/laporeon/meliscraper/repositories/SnapshotRepository.java
@@ -2,6 +2,7 @@ package com.laporeon.meliscraper.repositories;
 
 import com.laporeon.meliscraper.entities.Snapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,6 +12,6 @@ import java.util.UUID;
 @Repository
 public interface SnapshotRepository extends JpaRepository<Snapshot, UUID> {
 
-    Optional<Snapshot> findBySnapshotDate(LocalDate date);
+    Optional<Snapshot> findBySnapshotDate(@Param("date") LocalDate date);
 
 }
