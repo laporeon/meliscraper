61da46e3e6f3d0376a71b6fa35ba327102a54a8b
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-02 11:56:32 -0300

    feat(entities): create product entity

---
+++ src/main/java/com/laporeon/meliscraper/entities/Product.java
@@ -0,0 +1,46 @@
+package com.laporeon.meliscraper.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Integer position;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String link;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "snapshot_id", nullable = false)
+    private Snapshot snapshot;
+}
