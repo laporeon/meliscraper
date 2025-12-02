7d116b7d4e83a16682db39fe7c08341a8cad1ac2
laporeon <175569928+laporeon@users.noreply.github.com>
2025-12-02 11:53:30 -0300

    refactor(entities): remove ManyToMany relationship and createdAt from Snapshot entity

--- src/main/java/com/laporeon/meliscraper/entities/Snapshot.java
+++ src/main/java/com/laporeon/meliscraper/entities/Snapshot.java
@@ -5,13 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -31,13 +28,4 @@ public class Snapshot {
     @Column(name = "snapshot_date", nullable = false, updatable = false)
     private LocalDate snapshotDate;
 
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-    @ManyToMany
-    @JoinTable(name = "snapshot_categories",
-            joinColumns = @JoinColumn(name = "snapshot_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private List<Category> categories;
 }
