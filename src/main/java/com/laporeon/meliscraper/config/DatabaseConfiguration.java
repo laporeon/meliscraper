a791b6ba83cd1a53696ca96cd47dbf0b70c23731
laporeon <175569928+laporeon@users.noreply.github.com>
2026-04-23 18:00:09 -0300

    refactor(db): enable jpa repositories in db config file

--- src/main/java/com/laporeon/meliscraper/config/DatabaseConfiguration.java
+++ src/main/java/com/laporeon/meliscraper/config/DatabaseConfiguration.java
@@ -2,8 +2,10 @@ package com.laporeon.meliscraper.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.laporeon.meliscraper.repositories")
 public class DatabaseConfiguration {
 }
